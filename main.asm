ab546d5cf51bd31e30aaa5a5a7e3df9390c1c285
Dario Sigot <dario.sigot@gmail.com>
2023-05-22 20:35:22 +0200

    Menage AI

--- main.asm
+++ main.asm
@@ -48832,46 +48832,46 @@ TrainerClassMoveChoiceModifications: ; 3989b (e:589b)
     db 1,0     ; JR__TRAINER_M
     db 1,0     ; JR__TRAINER_F
     db 1,2,3,0 ; POKEMANIAC
-    db 1,2,0   ; SUPER_NERD
+    db 1,2,3,0 ; SUPER_NERD
     db 1,0     ; HIKER
     db 1,0     ; BIKER
     db 1,3,0   ; BURGLAR
-    db 1,2,3,0 ; ENGINEER
-    db 1,2,0   ; JUGGLER_X
+    db 1,3,0   ; ENGINEER
+    db 0       ; JUGGLER_X
     db 1,3,0   ; FISHER
     db 1,3,0   ; SWIMMER
     db 0       ; CUE_BALL
-    db 1,0     ; GAMBLER
+    db 1,3,0   ; GAMBLER
     db 1,3,0   ; BEAUTY
     db 1,2,3,0 ; PSYCHIC_TR
     db 1,3,0   ; ROCKER
-    db 1,0     ; JUGGLER
-    db 1,0     ; TAMER
+    db 1,2,3,0 ; JUGGLER
+    db 1,3,0   ; TAMER
     db 1,0     ; BIRD_KEEPER
     db 1,0     ; BLACKBELT
-    db 1,2,3,0 ; SONY1
+    db 1,3,0   ; SONY1
     db 0       ; PROF_OAK
     db 0       ; CHIEF
     db 1,2,3,0 ; SCIENTIST
-    db 1,2,3,0 ; GIOVANNI
+    db 1,3,0   ; GIOVANNI
     db 1,0     ; ROCKET
-    db 1,2,3,0 ; COOLTRAINER_M
-    db 1,2,3,0 ; COOLTRAINER_F
-    db 1,2,3,0 ; BRUNO
-    db 1,2,3,0 ; BROCK
-    db 1,2,3,0 ; MISTY
-    db 1,2,3,0 ; LT__SURGE
-    db 1,2,3,0 ; ERIKA
-    db 1,2,3,0 ; KOGA
-    db 1,2,3,0 ; BLAINE
-    db 1,2,3,0 ; SABRINA
+    db 1,3,0   ; COOLTRAINER_M
+    db 1,3,0   ; COOLTRAINER_F
+    db 1,3,0   ; BRUNO
+    db 1,3,0   ; BROCK
+    db 1,3,0   ; MISTY
+    db 1,3,0   ; LT__SURGE
+    db 1,3,0   ; ERIKA
+    db 1,3,0   ; KOGA
+    db 1,3,0   ; BLAINE
+    db 1,3,0   ; SABRINA
     db 1,2,3,0 ; GENTLEMAN
-    db 1,2,3,0 ; SONY2
-    db 1,2,3,0 ; SONY3
+    db 1,3,0   ; SONY2
+    db 1,3,0   ; SONY3
     db 1,2,3,0 ; LORELEI
     db 1,0     ; CHANNELER
-    db 1,2,3,0 ; AGATHA
-    db 1,2,3,0 ; LANCE
+    db 1,3,0   ; AGATHA
+    db 1,3,0   ; LANCE
 
 SECTION "Func_3ad0e",ROMX[$6d0e],BANK[$e]
 
