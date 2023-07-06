2a65753f200d56ad07f283d25b783ec2480c2c19
Dario Sigot <dario.sigot@gmail.com>
2023-07-06 13:01:22 +0200

    Disable TradeMons (Voltorb/Ditto)

--- main.asm
+++ main.asm
@@ -96008,16 +96008,18 @@ Func_71b6a: ; 71b6a (1c:5b6a)
 
 TradeMons: ; 71b7b (1c:5b7b)
 ; givemonster,getmonster,textstring,nickname (11 bytes),14 bytes total
-    db NIDORINO,NIDORINA,0,"TERRY@@@@@@"
-    db ABRA,MR_MIME,0,"MARCEL@@@@@"
-    db BUTTERFREE,BEEDRILL,2,"CHIKUCHIKU@"
-    db PONYTA,SEEL,0,"SAILOR@@@@@"
-    db SPEAROW,FARFETCH_D,2,"DUX@@@@@@@@"
-    db SLOWBRO,LICKITUNG,0,"MARC@@@@@@@"
-    db POLIWHIRL,JYNX,1,"LOLA@@@@@@@"
-    db RAICHU,ELECTRODE,1,"DORIS@@@@@@"
-    db VENONAT,TANGELA,2,"CRINKLES@@@"
-    db NIDORAN_M,NIDORAN_F,2,"SPOT@@@@@@@"
+    db VOLTORB,DITTO,0,"GUMMY@@@@@@"
+    db VOLTORB,DITTO,0,"GUMMY@@@@@@"
+    db VOLTORB,DITTO,2,"GUMMY@@@@@@"
+    db VOLTORB,DITTO,0,"GUMMY@@@@@@"
+    db VOLTORB,DITTO,2,"GUMMY@@@@@@"
+    db VOLTORB,DITTO,0,"GUMMY@@@@@@"
+    db VOLTORB,DITTO,1,"GUMMY@@@@@@"
+    db VOLTORB,DITTO,1,"GUMMY@@@@@@"
+    db VOLTORB,DITTO,2,"GUMMY@@@@@@"
+    db VOLTORB,DITTO,2,"GUMMY@@@@@@"
+
+SECTION "Func_71c07",ROMX[$5c07],BANK[$1c]
 
 Func_71c07: ; 71c07 (1c:5c07)
     xor a
