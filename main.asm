2fb5c7ab0f0dc37b8727fd235d0fac3697cc2a18
Dario Sigot <dario.sigot@gmail.com>
2024-05-20 11:57:23 +0200

    Edit Type Effectiveness

--- main.asm
+++ main.asm
@@ -56682,7 +56682,12 @@ DrawCurrentMonGenderInBattle:
     ld hl,Coord
     ret
 
-; Free Space
+ResetBattleFlagAndLoadCurrentOpponent:
+    xor a
+    ld [wUnusedC000],a;joenote - clear custom ai bits and battle flags at battle start
+    ld [wUnusedD366],a;joenote - clear ai switch tracker bits
+    ld a,[W_CUROPPONENT] ; $d059
+    ret
 
 SECTION "HandleCounterMove",ROMX[$6093],BANK[$f]
 
@@ -57317,40 +57322,10 @@ TypeEffects:
 ;     20 is ×2.0
 ;     05 is ×0.5
 ;     00 is ×0
-    db WATER,FIRE,20
-    db FIRE,GRASS,20
-    db FIRE,ICE,20
-    db GRASS,WATER,20
-    db ELECTRIC,WATER,20
-    db WATER,ROCK,20
-    db GROUND,FLYING,00
-    db WATER,WATER,05
-    db FIRE,FIRE,05
-    db ELECTRIC,ELECTRIC,05
-    db ICE,ICE,05
-    db GRASS,GRASS,05
-    db PSYCHIC,PSYCHIC,05
-    db FIRE,WATER,05
-    db GRASS,FIRE,05
-    db WATER,GRASS,05
-    db ELECTRIC,GRASS,05
+
     db NORMAL,ROCK,05
     db NORMAL,GHOST,00
-    db GHOST,GHOST,20
-    db FIRE,BUG,20
-    db FIRE,ROCK,05
-    db WATER,GROUND,20
-    db ELECTRIC,GROUND,00
-    db ELECTRIC,FLYING,20
-    db GRASS,GROUND,20
-    db GRASS,BUG,05
-    db GRASS,POISON,05
-    db GRASS,ROCK,20
-    db GRASS,FLYING,05
-    db ICE,WATER,05
-    db ICE,GRASS,20
-    db ICE,GROUND,20
-    db ICE,FLYING,20
+
     db FIGHTING,NORMAL,20
     db FIGHTING,POISON,05
     db FIGHTING,FLYING,05
@@ -57359,46 +57334,92 @@ TypeEffects:
     db FIGHTING,ROCK,20
     db FIGHTING,ICE,20
     db FIGHTING,GHOST,00
+
+    db FLYING,ELECTRIC,05
+    db FLYING,FIGHTING,20
+    db FLYING,BUG,20
+    db FLYING,GRASS,20
+    db FLYING,ROCK,05
+    db FLYING,GHOST,20
+
     db POISON,GRASS,20
     db POISON,POISON,05
     db POISON,GROUND,05
     db POISON,BUG,20
     db POISON,ROCK,05
-    db POISON,GHOST,05
+    db POISON,FIGHTING,20
+
     db GROUND,FIRE,20
     db GROUND,ELECTRIC,20
     db GROUND,GRASS,05
     db GROUND,BUG,05
     db GROUND,ROCK,20
     db GROUND,POISON,20
-    db FLYING,ELECTRIC,05
-    db FLYING,FIGHTING,20
-    db FLYING,BUG,20
-    db FLYING,GRASS,20
-    db FLYING,ROCK,05
-    db PSYCHIC,FIGHTING,20
-    db PSYCHIC,POISON,20
-    db BUG,FIRE,05
-    db BUG,GRASS,20
-    db BUG,FIGHTING,05
-    db BUG,FLYING,05
-    db BUG,PSYCHIC,20
-    db BUG,GHOST,05
-    db BUG,POISON,20
+    db GROUND,FLYING,00
+
     db ROCK,FIRE,20
     db ROCK,FIGHTING,05
     db ROCK,GROUND,05
     db ROCK,FLYING,20
     db ROCK,BUG,20
     db ROCK,ICE,20
-    db GHOST,NORMAL,00
-    db GHOST,PSYCHIC,00
+
+    db BUG,FIRE,05
+    db BUG,GRASS,20
+    db BUG,FIGHTING,05
+    db BUG,FLYING,05
+    db BUG,PSYCHIC,20
+    db BUG,POISON,05
+
+    db GHOST,PSYCHIC,20
+
+    db FIRE,FIRE,05
+    db FIRE,GRASS,20
+    db FIRE,ICE,20
+    db FIRE,WATER,05
+    db FIRE,BUG,20
+    db FIRE,ROCK,05
     db FIRE,DRAGON,05
+
+    db WATER,WATER,05
+    db WATER,FIRE,20
+    db WATER,ROCK,20
+    db WATER,GRASS,05
+    db WATER,GROUND,20
     db WATER,DRAGON,05
-    db ELECTRIC,DRAGON,05
+
+    db GRASS,GRASS,05
+    db GRASS,WATER,20
+    db GRASS,FIRE,05
+    db GRASS,GROUND,20
+    db GRASS,BUG,05
+    db GRASS,POISON,05
+    db GRASS,ROCK,20
+    db GRASS,FLYING,05
     db GRASS,DRAGON,05
+
+    db ELECTRIC,WATER,20
+    db ELECTRIC,ELECTRIC,05
+    db ELECTRIC,GRASS,05
+    db ELECTRIC,GROUND,00
+    db ELECTRIC,FLYING,20
+    db ELECTRIC,DRAGON,05
+
+    db PSYCHIC,PSYCHIC,05
+    db PSYCHIC,FIGHTING,20
+    db PSYCHIC,POISON,20
+    db PSYCHIC,GHOST,05
+    db PSYCHIC,BUG,05
+
+    db ICE,ICE,05
+    db ICE,WATER,05
+    db ICE,GRASS,20
+    db ICE,GROUND,20
+    db ICE,FLYING,20
     db ICE,DRAGON,20
-    db DRAGON,DRAGON,20
+    db ICE,FIRE,05
+    db ICE,BUG,20
+    db ICE,FIGHTING,05
     db $FF
 
 HandleEnemySwitchingOrUsingAnItemAndThenExecutePlayerMove:
@@ -57409,13 +57430,6 @@ HandleEnemySwitchingOrUsingAnItemAndThenExecutePlayerMove:
     call Bankswitch
     call c,SetEnemyActedBit ;if carry was set from TrainerAI, set the bit indicating the ai trainer switched or used an item
     jp ExecutePlayerMove ; execute player move ;note: this function writes zero to H_WHOSETURN
-
-ResetBattleFlagAndLoadCurrentOpponent:
-    xor a
-    ld [wUnusedC000],a;joenote - clear custom ai bits and battle flags at battle start
-    ld [wUnusedD366],a;joenote - clear ai switch tracker bits
-    ld a,[W_CUROPPONENT] ; $d059
-    ret
 
 HandleMenuInputLockedDuringDebugEnemyMove:
     ld hl,wDebugEnemyMoveBit7
