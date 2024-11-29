9446283f28088af260817f04ee8ff4c48d57641e
Dario Sigot <dario.sigot@gmail.com>
2024-11-29 12:09:42 +0100

    Version 0.84.101

--- main.asm
+++ main.asm
@@ -14215,7 +14215,7 @@ DisplayOptionMenu: ; 5e8a (1:5e8a)
     call PlaySound ; play sound
     ret
 .Version
-    db "0.84.100@"
+    db "0.84.101@"
 .eraseOldMenuCursor
     ld [wTopMenuItemX],a
     call EraseMenuCursor
@@ -18172,7 +18172,7 @@ PrintDenimVersionAndSaveScreenTilesToBuffer2:
     jr nz,.Loop
     ret
 .Version
-    db "0.84.100"
+    db "0.84.101"
 .GBC
     db $41
 
