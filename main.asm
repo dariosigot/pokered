0916cb45659352a25805dd0e14e4c65e13238cde
Dario Sigot <dario.sigot@gmail.com>
2025-09-04 21:15:11 +0200

    TM58 (IcePunch) in SeafoamIsland3

--- main.asm
+++ main.asm
@@ -22414,6 +22414,7 @@ MapHS:
     db UNKNOWN_DUNGEON_4,$03,Show ; $F4 (Machamp)
     db UNKNOWN_DUNGEON_4,$04,Show ; $F5 (Golem)
     db UNKNOWN_DUNGEON_4,$05,Show ; $F6 (Gengar)
+    db SEAFOAM_ISLANDS_3,$03,Show ; $F7
     db $FF
 
 ; Free
@@ -69342,33 +69343,11 @@ Seafoam3HolesCoords: ; 464a9 (11:64a9)
 SeafoamIslands3TextPointers: ; 464ae (11:64ae)
     dw BoulderText
     dw BoulderText
+    dw PickupItemText
 
-SeafoamIslands3Object: ; 0x464b2 (size=72)
-    db $7d ; border tile
+; Free
 
-    db $7 ; warps
-    db $3,$5,$0,SEAFOAM_ISLANDS_2
-    db $d,$5,$0,SEAFOAM_ISLANDS_4
-    db $7,$d,$2,SEAFOAM_ISLANDS_2
-    db $f,$13,$3,SEAFOAM_ISLANDS_2
-    db $3,$19,$3,SEAFOAM_ISLANDS_4
-    db $b,$19,$5,SEAFOAM_ISLANDS_2
-    db $e,$19,$4,SEAFOAM_ISLANDS_4
-
-    db $0 ; signs
-
-    db $2 ; people
-    db SPRITE_BOULDER,$6 + 4,$12 + 4,$ff,$10,$1 ; person
-    db SPRITE_BOULDER,$6 + 4,$17 + 4,$ff,$10,$2 ; person
-
-    ; warp-to
-    EVENT_DISP $f,$3,$5 ; SEAFOAM_ISLANDS_2
-    EVENT_DISP $f,$d,$5 ; SEAFOAM_ISLANDS_4
-    EVENT_DISP $f,$7,$d ; SEAFOAM_ISLANDS_2
-    EVENT_DISP $f,$f,$13 ; SEAFOAM_ISLANDS_2
-    EVENT_DISP $f,$3,$19 ; SEAFOAM_ISLANDS_4
-    EVENT_DISP $f,$b,$19 ; SEAFOAM_ISLANDS_2
-    EVENT_DISP $f,$e,$19 ; SEAFOAM_ISLANDS_4
+SECTION "SeafoamIslands3Blocks",ROMX[$64fa],BANK[$11]
 
 SeafoamIslands3Blocks: ; 464fa (11:64fa)
     INCBIN "maps/seafoamislands3.blk"
@@ -71158,6 +71137,36 @@ BoulderOnSwitch4:
     ld a,$03 + 4 ; wispnote - We need to offset coordinates by 4
     ld [hl],a
     ret
+
+; ───────────────────────────────────────
+
+SeafoamIslands3Object:
+    db $7d ; border tile
+
+    db $7 ; warps
+    db $3,$5,$0,SEAFOAM_ISLANDS_2
+    db $d,$5,$0,SEAFOAM_ISLANDS_4
+    db $7,$d,$2,SEAFOAM_ISLANDS_2
+    db $f,$13,$3,SEAFOAM_ISLANDS_2
+    db $3,$19,$3,SEAFOAM_ISLANDS_4
+    db $b,$19,$5,SEAFOAM_ISLANDS_2
+    db $e,$19,$4,SEAFOAM_ISLANDS_4
+
+    db $0 ; signs
+
+    db $3 ; people
+    db SPRITE_BOULDER,$6 + 4,$12 + 4,$ff,$10,$1 ; person
+    db SPRITE_BOULDER,$6 + 4,$17 + 4,$ff,$10,$2 ; person
+    db SPRITE_BALL,02 + 4,26 + 4,$ff,$ff,$83,TM_58 ; item
+
+    ; warp-to
+    EVENT_DISP $f,$3,$5 ; SEAFOAM_ISLANDS_2
+    EVENT_DISP $f,$d,$5 ; SEAFOAM_ISLANDS_4
+    EVENT_DISP $f,$7,$d ; SEAFOAM_ISLANDS_2
+    EVENT_DISP $f,$f,$13 ; SEAFOAM_ISLANDS_2
+    EVENT_DISP $f,$3,$19 ; SEAFOAM_ISLANDS_4
+    EVENT_DISP $f,$b,$19 ; SEAFOAM_ISLANDS_2
+    EVENT_DISP $f,$e,$19 ; SEAFOAM_ISLANDS_4
 
 ; ───────────────────────────────────────
 
