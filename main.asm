b14bf24a903974039d0fd4510c299ff022ce2041
Dario Sigot <dario.sigot@gmail.com>
2026-01-15 10:17:11 +0100

    Remove "EvosMoves" Useless Commented Code

--- main.asm
+++ main.asm
@@ -6211,12 +6211,6 @@ TrainerWalkUpToPlayer_Bank0:
     ld hl,TrainerWalkUpToPlayer
     jp Bankswitch ; indirect jump to TrainerWalkUpToPlayer (56881 (15:6881))
 
-;LoadEvosMovesPointerTableByPokedex:
-;    ld [$d11e],a
-;    call IndexToPokedexAndRestoreD11E
-;    ld hl,EvosMovesPointerTable
-;    ret
-
 GetTileOffset:
     push hl
     push de
@@ -29647,20 +29641,6 @@ RedrawPartyMenu_: ; 12ce3 (4:6ce3)
     db "NOT ABLE@"
 .evolutionStoneMenu
     push hl
-    ;ld b,0
-    ;ld a,[$CF98] ; pokemon ID
-    ;ds 1 ; dec a ; 00MOD
-    ;call LoadEvosMovesPointerTableByPokedex
-    ;add a
-    ;rl b
-    ;ld c,a
-    ;add hl,bc
-    ;ld de,$CD6D
-    ;ld a,BANK(EvosMovesPointerTable)
-    ;ld bc,2
-    ;call FarCopyData
-    ;ld hl,$CD6D
-
     ld hl,W_MONHLEARNSETPOINTER ; pointer to learnset
     ld a,[hli]
     ld h,[hl]
@@ -50917,15 +50897,6 @@ LearnMoveCommon:
     ld [$d11e],a
     ret
 .GetEvosMoves
-    ;ld a,[wNewMonIdDuringLearnMove]
-    ; ds 1 ; dec a ; 00MOD
-    ;ld bc,$0
-    ;call LoadEvosMovesPointerTableByPokedex
-    ;add a
-    ;rl b
-    ;ld c,a
-    ;add hl,bc
-
     ld a,[wNewMonIdDuringLearnMove]
     ld [$d0b5],a
     ld [$cf91],a
@@ -51021,15 +50992,6 @@ Evolution_PartyMonLoop:
     ld a,c
     and a
     jp z,Evolution_PartyMonLoop
-    ;ld a,[$cee9]
-    ;ds 1 ; dec a ; 00MOD
-    ;ld b,$0
-    ;call LoadEvosMovesPointerTableByPokedex
-    ;add a
-    ;rl b
-    ;ld c,a
-    ;add hl,bc
-
     ld a,[$cee9]
     ld [$d0b5],a
     ld hl,W_PARTYMON1_MOVE2PP ; move2pp
@@ -51634,15 +51596,6 @@ WriteMonMoves:
     push hl
     push de
     push bc
-    ;ld b,$0
-    ;ld a,[$cf91]  ; cur mon ID
-    ;call LoadEvosMovesPointerTableByPokedex
-    ;ds 1 ; dec a ; 00MOD
-    ;add a
-    ;rl b
-    ;ld c,a
-    ;add hl,bc
-
     ld hl,W_MONHLEARNSETPOINTER ; pointer to learnset
     ld a,[hli]
     ld h,[hl]
@@ -52377,168 +52330,6 @@ GetEvosMoves:
     ld hl,GenericBuffer+1
     pop de
     ret
-
-;EvosMovesPointerTable:
-;    dw MissingNo_EvosMoves
-;    dw Mon001_EvosMoves
-;    dw Mon002_EvosMoves
-;    dw Mon003_EvosMoves
-;    dw Mon004_EvosMoves
-;    dw Mon005_EvosMoves
-;    dw Mon006_EvosMoves
-;    dw Mon007_EvosMoves
-;    dw Mon008_EvosMoves
-;    dw Mon009_EvosMoves
-;    dw Mon010_EvosMoves
-;    dw Mon011_EvosMoves
-;    dw Mon012_EvosMoves
-;    dw Mon013_EvosMoves
-;    dw Mon014_EvosMoves
-;    dw Mon015_EvosMoves
-;    dw Mon016_EvosMoves
-;    dw Mon017_EvosMoves
-;    dw Mon018_EvosMoves
-;    dw Mon019_EvosMoves
-;    dw Mon020_EvosMoves
-;    dw Mon021_EvosMoves
-;    dw Mon022_EvosMoves
-;    dw Mon023_EvosMoves
-;    dw Mon024_EvosMoves
-;    dw Mon025_EvosMoves
-;    dw Mon026_EvosMoves
-;    dw Mon027_EvosMoves
-;    dw Mon028_EvosMoves
-;    dw Mon029_EvosMoves
-;    dw Mon030_EvosMoves
-;    dw Mon031_EvosMoves
-;    dw Mon032_EvosMoves
-;    dw Mon033_EvosMoves
-;    dw Mon034_EvosMoves
-;    dw Mon035_EvosMoves
-;    dw Mon036_EvosMoves
-;    dw Mon037_EvosMoves
-;    dw Mon038_EvosMoves
-;    dw Mon039_EvosMoves
-;    dw Mon040_EvosMoves
-;    dw Mon041_EvosMoves
-;    dw Mon042_EvosMoves
-;    dw Mon043_EvosMoves
-;    dw Mon044_EvosMoves
-;    dw Mon045_EvosMoves
-;    dw Mon046_EvosMoves
-;    dw Mon047_EvosMoves
-;    dw Mon048_EvosMoves
-;    dw Mon049_EvosMoves
-;    dw Mon050_EvosMoves
-;    dw Mon051_EvosMoves
-;    dw Mon052_EvosMoves
-;    dw Mon053_EvosMoves
-;    dw Mon054_EvosMoves
-;    dw Mon055_EvosMoves
-;    dw Mon056_EvosMoves
-;    dw Mon057_EvosMoves
-;    dw Mon058_EvosMoves
-;    dw Mon059_EvosMoves
-;    dw Mon060_EvosMoves
-;    dw Mon061_EvosMoves
-;    dw Mon062_EvosMoves
-;    dw Mon063_EvosMoves
-;    dw Mon064_EvosMoves
-;    dw Mon065_EvosMoves
-;    dw Mon066_EvosMoves
-;    dw Mon067_EvosMoves
-;    dw Mon068_EvosMoves
-;    dw Mon069_EvosMoves
-;    dw Mon070_EvosMoves
-;    dw Mon071_EvosMoves
-;    dw Mon072_EvosMoves
-;    dw Mon073_EvosMoves
-;    dw Mon074_EvosMoves
-;    dw Mon075_EvosMoves
-;    dw Mon076_EvosMoves
-;    dw Mon077_EvosMoves
-;    dw Mon078_EvosMoves
-;    dw Mon079_EvosMoves
-;    dw Mon080_EvosMoves
-;    dw Mon081_EvosMoves
-;    dw Mon082_EvosMoves
-;    dw Mon083_EvosMoves
-;    dw Mon084_EvosMoves
-;    dw Mon085_EvosMoves
-;    dw Mon086_EvosMoves
-;    dw Mon087_EvosMoves
-;    dw Mon088_EvosMoves
-;    dw Mon089_EvosMoves
-;    dw Mon090_EvosMoves
-;    dw Mon091_EvosMoves
-;    dw Mon092_EvosMoves
-;    dw Mon093_EvosMoves
-;    dw Mon094_EvosMoves
-;    dw Mon095_EvosMoves
-;    dw Mon096_EvosMoves
-;    dw Mon097_EvosMoves
-;    dw Mon098_EvosMoves
-;    dw Mon099_EvosMoves
-;    dw Mon100_EvosMoves
-;    dw Mon101_EvosMoves
-;    dw Mon102_EvosMoves
-;    dw Mon103_EvosMoves
-;    dw Mon104_EvosMoves
-;    dw Mon105_EvosMoves
-;    dw Mon106_EvosMoves
-;    dw Mon107_EvosMoves
-;    dw Mon108_EvosMoves
-;    dw Mon109_EvosMoves
-;    dw Mon110_EvosMoves
-;    dw Mon111_EvosMoves
-;    dw Mon112_EvosMoves
-;    dw Mon113_EvosMoves
-;    dw Mon114_EvosMoves
-;    dw Mon115_EvosMoves
-;    dw Mon116_EvosMoves
-;    dw Mon117_EvosMoves
-;    dw Mon118_EvosMoves
-;    dw Mon119_EvosMoves
-;    dw Mon120_EvosMoves
-;    dw Mon121_EvosMoves
-;    dw Mon122_EvosMoves
-;    dw Mon123_EvosMoves
-;    dw Mon124_EvosMoves
-;    dw Mon125_EvosMoves
-;    dw Mon126_EvosMoves
-;    dw Mon127_EvosMoves
-;    dw Mon128_EvosMoves
-;    dw Mon129_EvosMoves
-;    dw Mon130_EvosMoves
-;    dw Mon131_EvosMoves
-;    dw Mon132_EvosMoves
-;    dw Mon133_EvosMoves
-;    dw Mon134_EvosMoves
-;    dw Mon135_EvosMoves
-;    dw Mon136_EvosMoves
-;    dw Mon137_EvosMoves
-;    dw Mon138_EvosMoves
-;    dw Mon139_EvosMoves
-;    dw Mon140_EvosMoves
-;    dw Mon141_EvosMoves
-;    dw Mon142_EvosMoves
-;    dw Mon143_EvosMoves
-;    dw Mon144_EvosMoves
-;    dw Mon145_EvosMoves
-;    dw Mon146_EvosMoves
-;    dw Mon147_EvosMoves
-;    dw Mon148_EvosMoves
-;    dw Mon149_EvosMoves
-;    dw Mon150_EvosMoves
-;    dw Mon151_EvosMoves
-;    dw Mon152_EvosMoves
-;    dw Mon153_EvosMoves
-;    dw Mon154_EvosMoves
-;    dw MissingNo_EvosMoves ; 155
-;    dw MissingNo_EvosMoves ; 156
-;    dw MissingNo_EvosMoves ; 157
-;    dw MissingNo_EvosMoves ; 158
-;    dw MissingNo_EvosMoves ; 159
 
 CryData:
     ;$BaseCry,$Pitch,$Length
